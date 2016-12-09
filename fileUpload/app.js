6d7e061275c71b6301fa7cba3f59303b11f59d0c
ARahaman <argoodfrnd2u@gmail.com>
2016-12-09 21:55:20 +0530

    Any file can be uploaded

--- fileUpload/app.js
+++ fileUpload/app.js
@@ -1,23 +1,20 @@
 var express = require('express');
-var fileUpload = require('express-fileupload');
 var app = express();
-var fs = require('fs');		
+var fileUpload = require('express-fileupload');
 
 app.use(fileUpload());
-
-app.use(express.static(__dirname + '/'));
  
 app.post('/upload', function(req, res) {
-    
-console.log(req.body);
-
-if(!!req.body){
-		var name = req.body['file[file][name]'];
-		var data = req.body['file[file][data]'];
-	 	fs.writeFile('./uploads/'+name, data, 'binary', function() {
-		  res.end("File Uploaded");
-		});
-	}
+	var file = req.files.file;
+	var name = req.body.name;
+	file.mv('./uploads/'+name, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.send('File uploaded!');
+        }
+    });
 });
 
 app.listen(3000);
